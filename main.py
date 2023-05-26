5d72e8f1cb792d039240e9340bc61cf859f35d16
Kristina <ymnuc@mail.ru>
2023-05-26 16:18:21 +0300

    Bambytoys
    Parsing form site Bambytoys

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+headers =  { 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+}
+
+
+def array ():
+    N = 0
+    for count in range (1, 7):
+        sleep(3) #перерыв на 3 сек
+        url = f"https://www.bambytoys.ru/catalog/65_8_gaga_nast_igry_iz_vo_protiv_zavedeniya_klientami_ikh_produktsii_na_markerpleysy_/?PAGEN_1={count}" #проходим по страницам+
+
+
+
+        response = requests.get(url, headers=headers)  # получение ответа от сайта
+
+        soup = BeautifulSoup(response.text, "lxml")  # html.parser - доп вариант помощи bs4
+
+        data = soup.find_all("div", class_="bx_catalog_item double")  # находим как оформлены нужные контейнеры
+
+        for i in data:
+            N += 1
+            name = i.find("div", class_="bx_catalog_item_title").text.split('"')[1] #выводим только название в ковычках
+            price = float(i.find("div", class_ = "bx_catalog_item_price").text.replace(" ", "")[:-5]) #убираем лишний текст и пробелы
+            volume = i.find("div", class_ = "bx_catalog_item_count").text.strip()
+            picture = i.find ("a", class_ ="bx_catalog_item_images").get("href")
+            yield N, name, price, volume, picture
+
---
+++ save_to_excel.py
@@ -0,0 +1,35 @@
+import xlsxwriter
+from main import array
+
+def writer(parametr):
+    book = xlsxwriter.Workbook(r"C:\Users\ymnuc\Desktop\bambytoys.xlsx")
+    page = book.add_worksheet("bambytoys_gaga")
+
+    row = 1
+    column = 0
+
+    #задаем название колонок
+    page.write(0, 0, "N")
+    page.write(0, 1, "name")
+    page.write(0, 2, "price")
+    page.write(0, 3, "volume")
+    page.write(0, 4, "picture")
+
+    # устанавливает ширину колонки
+    page.set_column("A:A", 10)
+    page.set_column("B:B", 50)
+    page.set_column("C:C", 50)
+    page.set_column("D:D", 50)
+    page.set_column("E:E", 50)
+
+    for item in parametr():
+        page.write(row, column, item[0])
+        page.write(row, column+1, item[1])
+        page.write(row, column+2, item[2])
+        page.write(row, column+3, item[3])
+        page.write(row, column +4, item[4])
+        row +=1
+    book.close()
+
+
+writer(array)
